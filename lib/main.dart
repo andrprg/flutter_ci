941a896472fb5ff86e66985310f974e647af4b04
andrprg <andrprg@rambler.ru>
2021-04-16 15:50:19 +0300

    15-50

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,6 @@ s  @override
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
